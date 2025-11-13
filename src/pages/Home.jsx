773225df40fb318c4cc9215096544e173c010845
komal <dethekomalv@gmail.com>
2025-11-13 09:45:19 +0530

    Home page started and added context for the ShopContext and LatestCollection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatextCollection from '../components/LatextCollection'
 
 const Home = () => {
   return (
     <div>
-        
+        <Hero />
+        <LatextCollection/>
     </div>
   )
 }
